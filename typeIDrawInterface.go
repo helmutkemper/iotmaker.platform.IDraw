5f4b28abc61dd05bed7a4597115b5f6cfe11b99b
helmutkemper <helmut.kemper@ahgora.com.br>
2019-11-28 12:06:04 -0300

    trabalhando

--- typeIDrawInterface.go
+++ typeIDrawInterface.go
@@ -14,6 +14,9 @@ type IDraw interface {
 	ShadowColor(value color.RGBA)
 	ShadowOffsetX(value int)
 	ShadowOffsetY(value int)
+	AddColorStop(gradient interface{}, stop float64, color color.RGBA)
 	CreateLinearGradient(x0, y0, x1, y1 int) interface{}
 	CreateRadialGradient(x0, y0, r0, x1, y1, r1 int)
+	FillStyle(value interface{})
+	StrokeStyle(value interface{})
 }
