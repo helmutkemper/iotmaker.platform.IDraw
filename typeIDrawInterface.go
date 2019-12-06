753579f145b1eef0470dc790735f9fb318aeccad
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-05 22:59:32 -0300

    trabalhando

--- typeIDrawInterface.go
+++ typeIDrawInterface.go
@@ -187,6 +187,16 @@ type IDraw interface {
 	//     você omitir o método, o gradiente não será visivel. Você tem a obrigação de chamar o método pelo menos uma vez com uma cor
 	//     para que o gradiente seja visível.
 	AddColorStopPosition(gradient interface{}, stop float64, color color.RGBA)
+
+	// en: The fill() method fills the current drawing (path). The default color is black.
+	//     Tip: Use the fillStyle property to fill with another color/gradient.
+	//     Note: If the path is not closed, the fill() method will add a line from the last point to the start point of the
+	//     path to close the path (like closePath()), and then fill the path.
+	// pt_br: O método fill() preenche e pinta do desenho (caminho). A cor padrão é preto.
+	//     Dica: Use a propriedade fillStyle para adicionar uma cor ou gradient.
+	//     Nota: Se o caminho não estiver fechado, o método fill() irá adicioná uma linha do último ao primeiro ponto do
+	//     caminho para fechar o caminho (semelhante ao método closePath()) e só então irá pintar
+	Fill()
 	CreateLinearGradient(x0, y0, x1, y1 interface{}) interface{}
 	CreateRadialGradient(x0, y0, r0, x1, y1, r1 interface{}) interface{}
 	FillStyle(value interface{})
