b07dd49028e6ce53bfe122c3ba413c59f1e53756
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-07 20:30:22 -0300

    trabalhando

--- typeIDrawInterface.go
+++ typeIDrawInterface.go
@@ -492,5 +492,50 @@ type IDraw interface {
 	//     Dica: Use a propriedade fillStile() para determinar a cor, gradiente ou
 	//     padrão a ser usado no reenchimento.
 	FillRect(x, y, width, height int)
+
+	// en: Draws an image, canvas, or video onto the canvas
+	//     image: Specifies the image, canvas, or video element to use
+	//     sx: [optional] The x coordinate where to start clipping
+	//     sy: [optional] The y coordinate where to start clipping
+	//     sWidth: [optional] The width of the clipped image
+	//     sHeight: [optional] The height of the clipped image
+	//     x: The x coordinate where to place the image on the canvas
+	//     y: The y coordinate where to place the image on the canvas
+	//     width: [optional] The width of the image to use (stretch or reduce the
+	//            image)
+	//     height: [optional] The height of the image to use (stretch or reduce the
+	//     image)
+	//
+	//     Position the image on the canvas:
+	//     Golang Syntax: platform.DrawImage(img, x, y)
+	//
+	//     Position the image on the canvas, and specify width and height of the image:
+	//     Golang Syntax: platform.DrawImage(img, x, y, width, height)
+	//
+	//     Clip the image and position the clipped part on the canvas:
+	//     Golang Syntax: platform.drawImage(img, sx, sy, sWidth, sHeight, x, y, width,
+	//                    height)
+	//
+	// pt_br: Desenha uma imagem, canvas ou vídeo no elemento canvas
+	//     image: Especifica a imagem, canvas ou vídeo a ser usado
+	//     sx: [opcional] Coordenada x de onde o corte vai começar
+	//     sy: [opcional] Coordenada y de onde o corte vai começar
+	//     sWidth: [opcional] largura do corte
+	//     sHeight: [opcional] altura do corte
+	//     x: Coordenada x do canvas de onde o corte vai ser colocado
+	//     y: Coordenada y do canvas de onde o corte vai ser colocado
+	//     width: [opcional] Novo comprimento da imagem
+	//     height: [opcional] Nova largura da imagem
+	//
+	//     Posiciona a imagem no canvas
+	//     Golang Sintaxe: platform.DrawImage(img, x, y)
+	//
+	//     Posiciona a imagem no canvas e determina um novo tamanho da imagem final
+	//     Golang Sintaxe: platform.DrawImage(img, x, y, width, height)
+	//
+	//     Corta um pedaço da imagem e determina uma nova posição e tamanho para a
+	//     imagem final
+	//     Golang Sintaxe: platform.drawImage(img, sx, sy, sWidth, sHeight, x, y,
+	//                     width, height)
 	DrawImage(image interface{}, value ...int)
 }
--- typeIFilterGradientInterface.go
+++ typeIFilterGradientInterface.go
@@ -2,6 +2,29 @@ package iotmaker_platform_IDraw
 
 type IFilterGradientInterface interface {
 	PrepareFilter(platform ICanvasGradient)
+
+	// en: The fill() method fills the current drawing (path). The default color is
+	//     black.
+	//     Tip: Use the fillStyle property to fill with another color/gradient.
+	//     Note: If the path is not closed, the fill() method will add a line from the
+	//     last point to the start point of the path to close the path (like
+	//     closePath()), and then fill the path.
+	//
+	// pt_br: O método fill() preenche e pinta do desenho (caminho). A cor padrão é
+	//     preto.
+	//     Dica: Use a propriedade fillStyle para adicionar uma cor ou gradient.
+	//     Nota: Se o caminho não estiver fechado, o método fill() irá adicioná uma
+	//     linha do último ao primeiro ponto do caminho para fechar o caminho
+	//     (semelhante ao método closePath()) e só então irá pintar
 	Fill(gradient interface{})
+
+	// en: The stroke() method actually draws the path you have defined with all those
+	//     moveTo() and lineTo() methods. The default color is black.
+	//     Tip: Use the strokeStyle property to draw with another color/gradient.
+	//
+	// pt_br: O método stroke() desenha o caminho definido com os métodos moveTo() e
+	//     lineTo() usando a cor padrão, preta.
+	//     Dica: Use a propriedade strokeStyle para desenhar com outra cor ou usar um
+	//     gradiente
 	Stroke(gradient interface{})
 }
