903aa47bee26a1d755db260336256c0a63af00d5
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-09 19:04:03 -0300

    trabalhando

--- typeIHtmlInterface.go
+++ typeIHtmlInterface.go
@@ -2,4 +2,5 @@ package iotmaker_platform_IDraw
 
 type IHtml interface {
 	NewImage(parent interface{}, propertiesList map[string]interface{}, waitLoad bool) interface{}
+	Append(document, element interface{})
 }
