9ab7e7090e3bfac84813822d1eb966da2392c2e4
Helmut Kemper <helmut.kemper@gmail.com>
2020-01-01 01:29:48 -0300

    fontawesome

--- typeIHtmlInterface.go
+++ typeIHtmlInterface.go
@@ -1,6 +1,9 @@
 package iotmaker_platform_IDraw
 
+import "github.com/helmutkemper/iotmaker.platform.webbrowser/Html"
+
 type IHtml interface {
-	NewImage(parent interface{}, propertiesList map[string]interface{}, waitLoad bool) interface{}
+	NewImage(parent interface{}, propertiesList map[string]interface{}, waitLoad bool) Html.Image
 	Append(document, element interface{})
+	Remove(document, element interface{})
 }
