8954adb4b9386bb6692693438c556f3d8c6cabd7
Helmut Kemper <helmut.kemper@gmail.com>
2021-06-20 21:51:55 -0300

    refactoring

--- typeIHtmlInterface.go
+++ typeIHtmlInterface.go
@@ -3,7 +3,7 @@ package iotmaker_platform_IDraw
 import "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/html"
 
 type IHtml interface {
-	NewImage(parent interface{}, propertiesList map[string]interface{}, waitLoad bool) Html.Image
+	NewImage(parent interface{}, propertiesList map[string]interface{}, waitLoad bool) html.Image
 	Append(document, element interface{})
 	Remove(document, element interface{})
 	GetDocumentWidth(document interface{}) int
